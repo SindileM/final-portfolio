ba9d8122f9fd876689aa97854cf65d4353dfb09e
Sindile <smakhathini24@gmail.com>
2022-04-11 15:45:46 +0200

    updates and changes

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -69,7 +69,7 @@ mounted(){
   left: 0;
   width: 100%;
   height: 100%;
-  background:  #149ddd;;
+  background:  #0da146;;
   clip-path: circle(150px at 80% 20%);
   transition: 0.5s ease-in-out;
 }
